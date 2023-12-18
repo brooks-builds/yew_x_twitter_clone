b14456eab0e6cce351b295a0028ebaa895699212
Brookzerker <brooks@brookzerker.com>
2023-12-18 08:33:16 -0700

    preparing to edit posts

--- src/components/one_post.rs
+++ src/components/one_post.rs
@@ -1,6 +1,6 @@
-use crate::router::Route;
+use crate::{components::textarea::BBTextArea, router::Route};
 use stylist::{style, yew::styled_component};
-use yew::{classes, html, Html, Properties};
+use yew::{classes, html, use_state, Callback, Html, Properties};
 use yew_router::prelude::Link;
 
 use crate::store::Post;
@@ -14,10 +14,14 @@ pub struct Props {
 
 #[styled_component]
 pub fn BBPost(props: &Props) -> Html {
+    let editing_state = use_state(|| false);
+
     let style = style!(
         r#"
             border: 3px solid skyblue;
             padding: 2rem;
+            display: flex;
+            flex-direction: column;
         "#
     )
     .unwrap();
@@ -39,11 +43,37 @@ pub fn BBPost(props: &Props) -> Html {
     )
     .unwrap();
 
+    let edit_button_style = style!(
+        r#"
+            margin-left: auto;
+        "#
+    )
+    .unwrap();
+
+    let edit_button_onclick = {
+        let editing_state = editing_state.clone();
+
+        Callback::from(move |_event| {
+            editing_state.set(!*editing_state);
+        })
+    };
+
     let post_html = html! {
             <div class={style.clone()}>
-                <p>{props.post.text.clone()}</p>
+                {
+                    if *editing_state {
+                        html! {
+                            <BBTextArea value={props.post.text.clone()}/>
+                        }
+                    } else {
+                        html! {
+                            <p>{props.post.text.clone()}</p>
+                        }
+                    }
+                }
                 <p>{"likes:"} {props.post.likes}</p>
                 <p>{"Responses:"} {props.post.response_count}</p>
+                <button class={edit_button_style} onclick={edit_button_onclick}>{if *editing_state {"save"} else {"edit"}}</button>
             </div>
     };
 
